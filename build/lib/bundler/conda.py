5eda82434daeab04eff34e1c4d58903ef358079d
Francois Visconte <f.visconte@criteo.com>
2016-12-24 11:31:34 +0100

    Remove build directory

--- build/lib/bundler/conda.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import urllib
-import tempfile
-import os
-
-def install_conda(cond_distribution_url):
-    tmpdir = tempfile.mkdtemp()
-    urllib.urlretrieve(cond_distribution_url, os.path.join(tmpdir, 'conda.sh'))
